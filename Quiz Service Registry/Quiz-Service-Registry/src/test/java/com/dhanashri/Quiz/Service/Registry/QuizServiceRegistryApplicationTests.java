1dcbd9d191943d3714b3deb3bccae5b79fdc26ee
DhanashriPhatak <dhanuphatak4@gmail.com>
2025-04-04 16:03:06 -0400

    Added Service Registry for Quiz Application

---
+++ Quiz Service Registry/Quiz-Service-Registry/src/test/java/com/dhanashri/Quiz/Service/Registry/QuizServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhanashri.Quiz.Service.Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
